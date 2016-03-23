599d3190baf38035726ef0c4b73f82b3ed58efcf
hayashidesu <katsuhiko.hayashi.847@gmail.com>
2016-03-23 18:46:08 +0900

    Initial Commit

---
+++ CollectionViewSample2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CollectionViewSample2
+//
+//  Created by 林克彦 on 2016/03/23.
+//  Copyright © 2016年 Hayashidesu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
